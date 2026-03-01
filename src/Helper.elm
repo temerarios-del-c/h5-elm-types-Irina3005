5b71b6639478e4e599736171aa6ccba025e2aa5d
Irina3005 <up230231@alumnos.upa.edu.mx>
2026-03-01 16:00:32 -0600

    Primer intento

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,56 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+auxiliar : Float -> GradeStatus
+
+
+auciliar num =
+    if num < 0 then
+        Pending
+
+    else if num < 7 then
+        Failed
+
+    else
+        Approved
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade lista =
+    List.map auxiliar lista
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+
+
+irportAction lista =
+    List.map airplaneScheduleAction lista
